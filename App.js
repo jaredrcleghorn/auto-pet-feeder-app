9bb923c56299c9244ba64782c994bff61177375c
Jared Cleghorn <jaredrcleghorn@gmail.com>
2020-08-19 00:23:25 -0500

    Modify the initial page

--- App.js
+++ App.js
@@ -5,17 +5,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Auto Pet Feeder</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
   },
+	title: {
+		fontSize: 30,
+	},
 });
